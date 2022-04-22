75da5384b2e9e8f2c33eb7265941b7891e0a5f15
Chenda Zhang <chenda1997@hotmail.com>
2022-04-22 20:06:49 +0200

    Update experiment.bash

--- experiment.bash
+++ experiment.bash
@@ -1,4 +1,4 @@
-python3 million_song_prep.py 4 0 &
-python3 million_song_prep.py 4 1 &
-python3 million_song_prep.py 4 2 &
-python3 million_song_prep.py 4 3 &
+python3 million_song_prep.py 26 0 &
+python3 million_song_prep.py 26 1 &
+python3 million_song_prep.py 26 2 &
+python3 million_song_prep.py 26 3 &
